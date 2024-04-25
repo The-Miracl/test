b4b452a6372651677cca894782877f869cda1477
the-Miracl <1479802876@qq.com>
2024-04-25 22:49:09 +0800

    备注内容

---
+++ CMakeFiles/main.dir/b.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/b.cpp.o: /root/test/b.cpp /usr/include/stdc-predef.h \
+ /root/test/b.h
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "b.h"
+using namespace std;
+
+int main()
+{
+    int a = 2;
+    cout << "hello world " << add(a, 2) << endl;
+}
